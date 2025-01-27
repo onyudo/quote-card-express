8981d33734b3b10620b1842b6c6d9a5d423e50be
“Martin <martin@forceandform.com>
2025-01-27 15:47:33 -0500

    Updated notes about code in server,js file after researching various lines.

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const port = 1776;
 // Serves the front-end content in the public directory
 app.use("", express.static(path.join(__dirname, "./public")));
 
+// The app.use() function adds a new middleware to the app.
+// Middleware functions are responsible for handling the request and crafting a response. 
+// Essentially, whenever a request hits your backend,
+// Express will execute the functions you passed to app.use() in order.
 app.use(express.json());
+// This middleware is for handling URL-encoded data (key-value pairs),
+// which can come from form submissions or other HTTP requests that send data this way (e.g., API clients).
 app.use(express.urlencoded({extended: false}));
 
 // Serves the whole app
